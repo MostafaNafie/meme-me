4ba5976a24fe33e04b078f6aa44833aead281987
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2019-10-28 15:52:08 +0200

    Move the view up when the bottom textfield is tapped

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
-    let textFieldsDelegate = TextFieldsDelegate()
+    var currentTappedTextField: UITextField?
     
+    // Set the style of the textfields
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
@@ -27,38 +28,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Setting the textfields
+        // Setup the textfields
         topTextField.text = "TOP"
         topTextField.defaultTextAttributes = memeTextAttributes
         topTextField.textAlignment = .center
-        topTextField.delegate = textFieldsDelegate
+        topTextField.delegate = self
         bottomTextField.text = "BOTTOM"
         bottomTextField.defaultTextAttributes = memeTextAttributes
         bottomTextField.textAlignment = .center
-        bottomTextField.delegate = textFieldsDelegate
+        bottomTextField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         // Disable the camera button if the device doesn't have a camera
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        // Subscribe to Keyboard notifications, to move the view when keyboard shows
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Unsubscribe from Keyboard notifications
+        unsubscribeFromKeyboardNotifications()
     }
     
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
-        
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = .camera
         present(imagePickerController, animated: true, completion: nil)
-        
     }
     
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
-        
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = .photoLibrary
         present(imagePickerController, animated: true, completion: nil)
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -74,5 +78,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        // Move the view up when the bottom textfield is tapped
+        if currentTappedTextField == bottomTextField {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        // Move the view back down
+        view.frame.origin.y = 0
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        // Subscribe to keyboardWillShowNotification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Subscribe to keyboardWillHideNotification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        // Unsubscribe from keyboardWillShowNotification
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Unsubscribe from keyboardWillHideNotification
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Check which textfield is tapped
+        currentTappedTextField = textField
+        // Clear default text when the user starts editing
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismiss the keyboard when the user presses enter
+        textField.resignFirstResponder()
+        return true
+    }
+   
 }
-
